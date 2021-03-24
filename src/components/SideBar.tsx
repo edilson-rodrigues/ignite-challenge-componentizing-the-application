74716631a6192eb4dcbed819e78c6ff0769f7385
Edilson Rodrigues <edarodrigs@gmail.com>
2021-03-24 20:54:44 -0300

    [challenge 2 - chapter 1] - componentizing the application

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
-  // Complete aqui
+import React, {useEffect, useState} from "react";
+import '../styles/sidebar.scss'
+import {Button} from "./Button";
+import {GenreResponseProps} from "./types";
+import {api} from "../services/api";
+
+interface SideBarProps {
+    selectedGenreId: number;
+    onClick: (id: number) => void;
+}
+
+export function SideBar(props: SideBarProps) {
+
+    const { selectedGenreId, onClick} = props;
+
+    const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+    useEffect(() => {
+        api.get<GenreResponseProps[]>('genres').then(response => {
+            setGenres(response.data);
+        });
+    }, []);
+
+    return (
+        <nav className="sidebar">
+            <span>Watch<p>Me</p></span>
+            <div className="buttons-container">
+                {genres.map(genre => (
+                    <Button
+                        key={String(genre.id)}
+                        title={genre.title}
+                        iconName={genre.name}
+                        onClick={() => onClick(genre.id)}
+                        selected={selectedGenreId === genre.id}
+                    />
+                ))}
+            </div>
+        </nav>
+    )
 }
